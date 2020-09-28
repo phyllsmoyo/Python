a9a3b5fd48cc05e54d1a79c756ec33c22a60b0be
Phillip Sinothi Moyo <phyllsmoyo@gmail.com>
2020-09-28 20:47:12 +0100

    Added else

--- Projects/tower of hanoi.py
+++ Projects/tower of hanoi.py
@@ -1,3 +1,15 @@
 # Code for solving the tower of Hanoi
 
-def hanoi(n, from, to, via)
+
+def hanoi(n, start, end, via):
+    if n == 1:
+        print("Move from", start, "to", end)
+
+    else:
+        hanoi(n - 1, start, via, end)
+        print("Move from", start, "to", end)
+        hanoi(n - 1, via, end, start)
+
+
+n = 1
+hanoi(n, "A", "B", "C")
