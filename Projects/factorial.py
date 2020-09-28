fe525d835b2bab008b947515c07eec1f81ec475b
Phillip Sinothi Moyo <phyllsmoyo@gmail.com>
2020-09-28 22:38:38 +0100

    included elif to handle negative numbers

---
+++ Projects/factorial.py
@@ -0,0 +1,19 @@
+# calculating factorial of a number
+# 0! = 1
+# 1! = 1
+# 2! = 2 * 1
+# n! = n * (n-1) * (n-2) * ... * 1
+
+# using recurssion i python
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    elif n < 1:
+        return "Try again, function only takes postive integers"
+    else:
+        # n * (factorial of n-1)
+        return n * (factorial(n - 1))
+
+
+n = -1
+print(factorial(n))
