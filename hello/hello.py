6906a4b63ccd9ae6c8b9ab7bbf7e83d0c5399584
Phillip Sinothi Moyo <phyllsmoyo@gmail.com>
2020-09-29 08:17:35 +0100

    Added more code

--- hello/hello.py
+++ hello/hello.py
@@ -1,1 +1,7 @@
-print("Hello World")
+a = "\u0030"  # unicode for 0
+b = "\u00B2"  # unicode for &sup2;
+c = "10km2"
+
+print(a.isnumeric())
+print(b.isnumeric())
+print(c.isnumeric())
---
+++ python-format-strings/exercise2.py
@@ -0,0 +1,58 @@
+# message = str.capitalize("first message")
+# print(message)
+
+# message = "second message".capitalize()
+# print(message)
+
+# message = "third message"
+# print(message.capitalize())
+
+# Changing case from UPPER to lower
+# message = "hello world"
+# print(message.lower())
+# print(message.upper())
+
+# message = message.title()
+# print(message)
+# print(message.swapcase())
+
+# Count the number of characters in a string
+
+# location = "Mississippi"
+# print(location.count("s"))
+
+# message = "racecar"
+# print(message.startswith("r"))
+# print(message.startswith("a"))
+# print(message.startswith("ra"))
+
+# print(message.endswith("r"))
+# print(message.endswith("a"))
+# print(message.endswith("ar"))
+
+# step 6
+# message = "The quick brown fox jumps over the lazy dog"
+# print(message.find("q"))
+
+# print(message.find("t"))
+# print(message.find("T"))
+# print(message.find("br"))
+
+# step 7
+# message = "    middle     "
+# print("." + message.lstrip() + ".")
+# print("." + message.rstrip() + ".")
+# print("." + message.strip() + ".")
+
+# # Step 8: Replace
+# message = "brevity is the essence of wit"
+# message = message.replace("essence", "soul")
+# print(message)
+
+# Step 9: Justify
+message = "howdy"
+message2 = "Sinothi"
+print(message.rjust(20))
+print(message2.rjust(20, "-"))
+print(message.ljust(20))
+print(message.ljust(20, "_"))
