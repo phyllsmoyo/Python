eb486cbbeae7c607aea897ba0992e47b8b5c610c
Phillip Sinothi Moyo <phyllsmoyo@gmail.com>
2020-10-25 22:51:07 +0200

    Completed Ex1

--- python-format-strings/exercise1.py
+++ python-format-strings/exercise1.py
@@ -2,7 +2,39 @@
 # second_string = "A literal string"
 # print(second_string == first_string)
 
-third_string = 'A single quoted literal string with a " double quote'
-fourth_string = "A double quoted literal string with a ' single quote"
-print(third_string)
-print(fourth_string)
+# third_string = 'A single quoted literal string with a " double quote'
+# fourth_string = "A double quoted literal string with a ' single quote"
+# print(third_string)
+# print(fourth_string)
+
+# ixth_string = 'A double quoted literal string with a " double quote'
+# seventh_string = "A literal string with a \n new line character"
+# eighth_string = "A literal string with a \t tab character"
+
+# print(fifth_string)
+# print(sixth_string)
+# print(seventh_string)
+# print(eighth_string)
+
+# nineth_string = r"A literal string with a \n new line printed raw"
+# print(nineth_string)
+
+# tenth_string = """A literal string
+# on more than one line
+# sometimes known as verbatim string"""
+
+# eleventh_string = """Another literal string
+#     on more than one line
+# using double quotes"""
+
+# print(tenth_string)
+# print(eleventh_string)
+
+first = "Phillip"
+second = "Sinothi"
+third = "Ntombi"
+print(first, second)
+print(first, second, third)
+print(first, second, third, sep="-")
+print(first, second, third, sep="-", end=".")
+
